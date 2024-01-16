36a46a9d4f4af6bc072964a1f893d325b26d1362
Ahmed khaled <ahmedkh8907@gmail.com>
2024-01-16 21:37:02 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'features/splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      title: 'Bookly',
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+    );
+  }
+}
